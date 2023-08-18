5cd34c0f772714fd46e7ded8dc6e1f058fadb7a8
Denis Halimaj <denis@imbus.local>
2023-08-18 14:17:52 +0200

    Changed database and environment moved to docker

--- app/database.py
+++ app/database.py
@@ -1,37 +1,19 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = 'postgresql://postgres:denis1234?@localhost:5432/DispoDB'
-# engine = create_engine(DATABASE_URL)
-
-
-# SessionLocal = sessionmaker(bind=engine,autoflush=False, autocommit= False, bind= engine)
-
-# Base = declarative_base()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()  
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:password@db:5432/book_db'
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine,autoflush=False, autocommit= False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()      
+        db.close()     
