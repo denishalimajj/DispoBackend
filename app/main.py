84eef249d826e79f8c1c0d505d5d6dd67e327e2e
redon <redon_halimaj@hotmail.com>
2023-10-01 15:48:37 +0200

    add CORS policy to main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from . import models
 from app.database import  engine
 from .router import authentication, user, vehicle
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust the origins accordingly
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(authentication.router)
 app.include_router(user.router)
