89b7446bdb13a52178c7151f70bbbebcc05c168f
denishalimajj <denis.halimaj11@gmail.com>
2023-11-02 18:52:21 +0100

    fixed docker and authentication

--- app/router/authentication.py
+++ app/router/authentication.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: schemas.Login = Depends(OAuth2PasswordRequestForm), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
          raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid credentials")
